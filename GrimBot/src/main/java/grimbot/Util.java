a4607b84d409f8189b77dc3f821f7c5a26e9caa3
marzipanic <ticklemepinkish@gmail.com>
2017-04-10 01:46:31 -0500

    Removed unused code

--- GrimBot/src/main/java/grimbot/Util.java
+++ GrimBot/src/main/java/grimbot/Util.java
@@ -59,20 +59,4 @@ public class Util {
 		}
 		return map;
 	}
-	
-	public static List<HashMap<String,Object>> getResultSetAsList(ResultSet rs) throws SQLException {
-	    ResultSetMetaData md = rs.getMetaData();
-	    int columns = md.getColumnCount();
-	    List<HashMap<String,Object>> list = new ArrayList<HashMap<String,Object>>();
-
-	    while (rs.next()) {
-	        HashMap<String,Object> row = new HashMap<String, Object>(columns);
-	        for(int i=1; i<=columns; ++i) {
-	            row.put(md.getColumnName(i),rs.getObject(i));
-	        }
-	        list.add(row);
-	    }
-
-	    return list;
-	}
 }
