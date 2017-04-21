0310fd6ae33212b55952d0e352586919376002b1
marzipanic <ticklemepinkish@gmail.com>
2017-04-21 15:45:42 -0500

    Rewrote isInteger()

--- GrimBot/src/main/java/grimbot/Util.java
+++ GrimBot/src/main/java/grimbot/Util.java
@@ -14,19 +14,12 @@ import java.util.List;
 public class Util {
 	
 	public static boolean isInteger(String s) {
-	    return isInteger(s,10);
-	}
-
-	public static boolean isInteger(String s, int radix) {
-	    if(s.isEmpty()) return false;
-	    for(int i = 0; i < s.length(); i++) {
-	        if(i == 0 && s.charAt(i) == '-') {
-	            if(s.length() == 1) return false;
-	            else continue;
-	        }
-	        if(Character.digit(s.charAt(i),radix) < 0) return false;
-	    }
-	    return true;
+		try {
+            Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException e){
+            return false;
+        }
 	}
 	
 	
